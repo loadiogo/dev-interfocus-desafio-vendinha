3e0567fe24e166e80c852de54529d684b693e018
Diogo Henrique <diogo.interfocus@outlook.com.br>
2021-06-17 23:10:29 -0300

    Adiciona arquivos backend

---
+++ backend/TemplateApi/Controllers/DividaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplateApi.Models;
+
+namespace TemplateApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DividaController : ControllerBase
+    {
+        public static List<Divida> ListaDividas = new List<Divida>();
+
+        public IActionResult Get()
+        {
+            throw new NotImplementedException("Escrever a implementação do GET");
+        }
+
+
+        public IActionResult Post([FromBody] Divida divida)
+        {
+            throw new NotImplementedException("Escrever a implementação do POST");
+        }
+
+
+        public IActionResult Put([FromBody] Divida divida)
+        {
+            throw new NotImplementedException("Escrever a implementação do PUT");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException("Escrever a implementação do DELETE");
+        }
+    }
+}
---
+++ backend/TemplateApi/Models/Divida.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemplateApi.Models
+{
+    public class Divida
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public bool Pago { get; set; }
+    }
+}
