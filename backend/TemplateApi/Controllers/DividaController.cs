0a8d4018027bb61fbae0a90c539f3d39cc3d1392
Diogo Henrique <diogo.interfocus@outlook.com.br>
2021-06-24 12:09:14 -0300

    Adiciona dataAnnotations nos metodos http

--- backend/TemplateApi/Controllers/DividaController.cs
+++ backend/TemplateApi/Controllers/DividaController.cs
@@ -14,24 +14,25 @@ namespace TemplateApi.Controllers
     {
         public static List<Divida> ListaDividas = new List<Divida>();
 
+        [HttpGet]
         public IActionResult Get()
         {
             throw new NotImplementedException("Escrever a implementação do GET");
         }
 
-
+        [HttpPost]
         public IActionResult Post([FromBody] Divida divida)
         {
             throw new NotImplementedException("Escrever a implementação do POST");
         }
 
-
+        [HttpPut]
         public IActionResult Put([FromBody] Divida divida)
         {
             throw new NotImplementedException("Escrever a implementação do PUT");
         }
 
-
+        [HttpDelete]
         public IActionResult Delete(int id)
         {
             throw new NotImplementedException("Escrever a implementação do DELETE");
